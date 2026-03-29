2629f851048327c793de2c93f4abdee4b9bb6921
Aaron Meche <aaronmeche18@icloud.com>
2026-03-29 16:16:40 -0500

    added full rue-lang compatibility, updated style to dark blue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
 	import '$lib/main.css';
+	import '$lib/main.rue';
 	let { children } = $props();
 
 </script>
 
 <svelte:head>
 	<!-- <link rel="icon" href={favicon} /> -->
-	<title>My Aapp</title>
+	<title>Aaron's App</title>
 	<script src="https://kit.fontawesome.com/5cf062dc93.js" crossorigin="anonymous"></script>
 </svelte:head>
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import ruePlugin from 'rue-lang/src/vite-plugin.js';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [ruePlugin(), sveltekit()]
 });
